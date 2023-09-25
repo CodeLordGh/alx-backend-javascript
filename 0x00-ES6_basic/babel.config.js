e7c734b35fb3cabb66d3df093fac51f07c5d4841
CodeLordGh <babohkossi228@icloud.com>
2023-09-25 15:45:56 +0000

    initial commit

---
+++ 0x00-ES6_basic/babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // eslint-disable-next-line linebreak-style
+  presets: [
+    // eslint-disable-next-line linebreak-style
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
