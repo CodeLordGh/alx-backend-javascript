7507bf317daff32aa2ec3fd324843d7e674897d0
CodeLordGh <babohkossi228@icloud.com>
2023-10-02 09:55:47 +0000

    get list of students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const listStudents = [
+    {
+      firstName: "Guillaume",
+      id: 1,
+      location: "San Francisco",
+    },
+    {
+      firstName: "James",
+      id: 2,
+      location: "Columbia",
+    },
+    {
+      firstName: "Serena",
+      id: 3,
+      location: "San Francisco",
+    },
+  ];
+
+  return listStudents;
+}
