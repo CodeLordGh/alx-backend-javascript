4b791ea5ca4e4190a592ee531cdc0076e975fb51
CodeLordGh <babohkossi228@icloud.com>
2023-11-24 15:26:16 +0000

    Updated the assertion library from 'assert' to 'assert/strict'.

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 // Import assert module
-import { equal } from "assert";
+import assert from "assert";
 
 // Import calculateNumber function
 import calculateNumber from "./0-calcul.js";
@@ -7,22 +7,22 @@ import calculateNumber from "./0-calcul.js";
 describe("calculateNumber", function () {
   // Test for positive integers
   it("should return the sum of rounded integers", function () {
-    equal(calculateNumber(1, 3), 4);
+    assert(calculateNumber(1, 3), 4);
   });
 
   // Test for rounding up
   it("should round up decimals", function () {
-    equal(calculateNumber(1, 3.7), 5);
+    assert(calculateNumber(1, 3.7), 5);
   });
 
   // Test for rounding down
   it("should round down decimals", function () {
-    equal(calculateNumber(1.2, 3.7), 5);
+    assert(calculateNumber(1.2, 3.7), 5);
   });
 
   // Test for rounding to nearest integer
   it("should round to nearest integer", function () {
-    equal(calculateNumber(1.5, 3.7), 6);
+    assert(calculateNumber(1.5, 3.7), 6);
   });
 
   // ...add any other test cases you want...
